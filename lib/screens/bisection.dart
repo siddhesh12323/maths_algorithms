cbbbd526d003077192e17007f96c85afafaf95a9
Siddhesh Kumbhar <purnakumbhar1234@gmail.com>
2023-05-16 01:36:54 +0530

    show iterations in bisection method

--- lib/screens/bisection.dart
+++ lib/screens/bisection.dart
@@ -18,8 +18,8 @@ class _BisectionState extends State<Bisection> {
   String iterations = 'No. of iterations will appear here';
   String timeTaken = "Algorithm's will appear here";
 
-  List<double> bisection(String func, double a, double b,
-      {int maxIterations = 1000, double tolerance = 1e-12}) {
+  List bisection(String func, double a, double b,
+      {int maxIterations = 500, double tolerance = 1e-12}) {
     //final stopwatch = Stopwatch()..start();
     Parser p1 = Parser();
     Expression exp = p1.parse(func);
@@ -31,6 +31,10 @@ class _BisectionState extends State<Bisection> {
     cm4.bindVariableName('x', Number(b));
     double c = 0;
     int iter = 0;
+    List valuesListC = [];
+    List valuesListB = [];
+    List valuesListA = [];
+    List valuesListBADiff = [];
     if (exp.evaluate(EvaluationType.REAL, cm3) *
             exp.evaluate(EvaluationType.REAL, cm4) <
         0) {
@@ -38,9 +42,19 @@ class _BisectionState extends State<Bisection> {
         c = (a + b) / 2;
         cm1.bindVariableName('x', Number(c));
         if (exp.evaluate(EvaluationType.REAL, cm1).abs() < tolerance) {
-          return [c, iter.toDouble()];
+          return [
+            c,
+            iter.toDouble(),
+            valuesListA,
+            valuesListB,
+            valuesListC,
+            valuesListBADiff
+          ];
         }
         cm2.bindVariableName('x', Number(a));
+        valuesListA.add(a);
+        valuesListB.add(b);
+        valuesListBADiff.add((b - a));
         if (exp.evaluate(EvaluationType.REAL, cm1) *
                 exp.evaluate(EvaluationType.REAL, cm2) <
             0) {
@@ -48,17 +62,31 @@ class _BisectionState extends State<Bisection> {
         } else {
           a = c;
         }
+        valuesListC.add(c);
         iter++;
       }
       //stopwatch.stop();
-      return [c, iter.toDouble()];
+      return [
+        c,
+        iter.toDouble(),
+        valuesListA,
+        valuesListB,
+        valuesListC,
+        valuesListBADiff
+      ];
     } else {
       //stopwatch.stop();
-      return [-1, 0];
+      return [-1, 0, valuesListA, valuesListB, valuesListC, valuesListBADiff];
     }
   }
 
-  List<double> ans = [];
+  List ans = [];
+  String valuesC = '';
+  String valuesA = '';
+  String valuesB = '';
+  String valuesBADiff = '';
+  List<DataRow> row1 = [];
+  List<DataColumn> column1 = [const DataColumn(label: Text('')),];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -93,121 +121,201 @@ class _BisectionState extends State<Bisection> {
         backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
         titleTextStyle: Theme.of(context).appBarTheme.titleTextStyle,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              decoration: InputDecoration(
-                  hintText: 'Enter the equation',
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        _textController.clear();
-                      },
-                      icon: const Icon(Icons.clear))),
-              controller: _textController,
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                    hintText: 'Enter the equation',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          _textController.clear();
+                        },
+                        icon: const Icon(Icons.clear))),
+                controller: _textController,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: 'Enter a',
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        _aController.clear();
-                      },
-                      icon: const Icon(Icons.clear))),
-              controller: _aController,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter a',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          _aController.clear();
+                        },
+                        icon: const Icon(Icons.clear))),
+                controller: _aController,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: 'Enter b',
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        _bController.clear();
-                      },
-                      icon: const Icon(Icons.clear))),
-              controller: _bController,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter b',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          _bController.clear();
+                        },
+                        icon: const Icon(Icons.clear))),
+                controller: _bController,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                  hintText: 'Enter the error factor',
-                  border: const OutlineInputBorder(),
-                  suffixIcon: IconButton(
-                      onPressed: () {
-                        _errorController.clear();
-                      },
-                      icon: const Icon(Icons.clear))),
-              controller: _errorController,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    hintText: 'Enter the error factor',
+                    border: const OutlineInputBorder(),
+                    suffixIcon: IconButton(
+                        onPressed: () {
+                          _errorController.clear();
+                        },
+                        icon: const Icon(Icons.clear))),
+                controller: _errorController,
+              ),
             ),
-          ),
-          TextButton(
-              style: Theme.of(context).textButtonTheme.style,
-              onPressed: () {
-                setState(() {
-                  try {
-                    ans = bisection(
-                        _textController.text,
-                        double.parse(_aController.text),
-                        double.parse(_bController.text),
-                        tolerance: double.parse(_errorController.text));
-                    output = 'Result:- ${ans[0].toString()}';
-                    iterations = 'Iterations:- ${ans[1].toInt().toString()}';
-                    //timeTaken = 'Execution Time:- ${ans[2].toString()}';
-                  } catch (e) {
-                    output = "Please enter double values only";
-                  }
-                });
-              },
-              child: const Text('Solve the equation')),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              child: Center(
-                  child: Text(
-                output,
-                style: Theme.of(context).textTheme.headlineSmall,
-              )),
+            TextButton(
+                style: Theme.of(context).textButtonTheme.style,
+                onPressed: () {
+                  setState(() {
+                    try {
+                      ans = bisection(
+                          _textController.text,
+                          double.parse(_aController.text),
+                          double.parse(_bController.text),
+                          tolerance: double.parse(_errorController.text));
+                      output = 'Result:- ${ans[0].toString()}';
+                      iterations = 'Iterations:- ${ans[1].toInt().toString()}';
+                      valuesA = ans[2].toString();
+                      valuesB = ans[3].toString();
+                      valuesC = ans[4].toString();
+                      valuesBADiff = ans[5].toString();
+                      row1 = displayRow(ans);
+                      column1 = displayColumn(ans);
+                      //timeTaken = 'Execution Time:- ${ans[2].toString()}';
+                    } catch (e) {
+                      output = "Please enter double values only";
+                    }
+                  });
+                },
+                child: const Text('Solve the equation')),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  output,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              child: Center(
-                  child: Text(
-                iterations,
-                style: Theme.of(context).textTheme.headlineSmall,
-              )),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  iterations,
+                  style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
             ),
-          ),
-          // Padding(
-          //   padding: const EdgeInsets.all(8.0),
-          //   child: Container(
-          //     child: Center(
-          //         child: Text(
-          //       timeTaken,
-          //       style: Theme.of(context).textTheme.headlineSmall,
-          //     )),
-          //   ),
-          // )
-        ],
+            // Padding(
+            //   padding: const EdgeInsets.all(8.0),
+            //   child: Container(
+            //     child: Center(
+            //         child: Text(
+            //       timeTaken,
+            //       style: Theme.of(context).textTheme.headlineSmall,
+            //     )),
+            //   ),
+            // )
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  valuesA,
+                  // style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  valuesB,
+                  // style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  valuesC,
+                  // style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                child: Center(
+                    child: Text(
+                  valuesBADiff,
+                  // style: Theme.of(context).textTheme.headlineSmall,
+                )),
+              ),
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DataTable(columns: column1,
+                 rows: row1),
+              ),
+            )
+          ],
+        ),
       ),
       //),
     );
+  }
+
+  List<DataRow> displayRow(List<dynamic> ans) {
+    List<DataRow> row = [];
+    for (int i = 0; i < ans[1].toInt(); i++) {
+      row.add(DataRow(cells: [
+        DataCell(Text((i + 1).toString())),
+        DataCell(Text(ans[2][i].toString())),
+        DataCell(Text(ans[3][i].toString())),
+        DataCell(Text(ans[4][i].toString())),
+        DataCell(Text(ans[5][i].toString())),
+      ]));
+    }
+    return row;
+  }
+
+  List<DataColumn> displayColumn(List<dynamic> ans) {
+    return const [
+      DataColumn(label: Text('Iteration No.')),
+      DataColumn(label: Text('a')),
+      DataColumn(label: Text('b')),
+      DataColumn(label: Text('c')),
+      DataColumn(label: Text('b  -  a'))
+    ];
   }
 }
